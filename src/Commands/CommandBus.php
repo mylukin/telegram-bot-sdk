aef077e84b97aa1ba1dcf90b5d55d613b8d7e6bb
Syed I.R <syed@lukonet.com>
2017-06-19 17:47:47 +0530

    Fix a Bug

--- src/Commands/CommandBus.php
+++ src/Commands/CommandBus.php
@@ -227,19 +227,19 @@ class CommandBus extends AnswerBus
     /**
      * Parse Command Arguments.
      *
-     * @param string $command
-     * @param array  $entity
-     * @param string $pattern
-     * @param string $text
+     * @param string  $command
+     * @param array   $entity
+     * @param Command $commandInstance
+     * @param string  $text
      *
      * @return array
      */
-    protected function parseCommandArguments(string $command, array $entity, string $pattern, string $text): array
+    protected function parseCommandArguments(string $command, array $entity, $commandInstance, string $text): array
     {
         $args = [];
 
         $paramPattern = '/\{((?:(?!\d+,?\d+?)\w)+?)\}/';
-
+        $pattern = $commandInstance->getPattern();
         $pattern = sprintf('/%s %s', $command, $pattern);
         $pattern = str_replace(['/', ' '], ['\/', '\s?'], $pattern);
 
@@ -249,8 +249,8 @@ class CommandBus extends AnswerBus
             array_shift($args);
         }
 
-        if(count($args) === 0) {
-            $method = new \ReflectionMethod($this, 'handle');
+        if (count($args) === 0 && method_exists($commandInstance, 'handle')) {
+            $method = new \ReflectionMethod($commandInstance, 'handle');
             $paramsCount = $method->getNumberOfParameters();
             $args = array_pad($args, $paramsCount, '');
         }
@@ -274,7 +274,7 @@ class CommandBus extends AnswerBus
             $this->commands['help'] ?? null;
 
         if ($command) {
-            $args = $this->parseCommandArguments($name, $entity, $command->getPattern(), $update->getMessage()->text);
+            $args = $this->parseCommandArguments($name, $entity, $command, $update->getMessage()->text);
 
             return $command->setArguments($args)->make($this->telegram, $update);
         }
